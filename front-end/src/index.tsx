984c2c0deb12ff4e9ba4ca54f3e22823ad74dcf9
David Rivera <jherax@gmail.com>
2024-01-11 22:23:14 -0500

    feat: Added form validations

--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -1,4 +1,5 @@
 import 'bulma/css/bulma.min.css';
+import './base.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import ProductLayout from './Products/components/ProductLayout';
